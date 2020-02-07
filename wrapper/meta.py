c25d5e96e872a9874a9fb4cb90d9b28afaf7d3f3
Tomáš Golembiovský <tgolembi@redhat.com>
2020-02-07 14:06:04 +0100

    build: have only one meta.py
    Signed-off-by: Tomáš Golembiovský <tgolembi@redhat.com>

---
+++ wrapper/meta.py
@@ -0,0 +1,10 @@
+""" Basic project metadata, all in one place """
+
+NAME = "virt-v2v-wrapper"
+VERSION = "2.0.1"
+RELEASE = "1"
+DESCRIPTION = "Tool for converting VMs using virt-v2v in an automated fashion"
+KEYWORDS = "v2v, kvm, vmware, conversion"
+AUTHOR = "Tomáš Golembiovský"
+EMAIL = "tgolembi@redhat.com"
+LICENSE = "Apache-2.0"

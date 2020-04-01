f79ff2e41792e0ae28797604b6e1efd7c89ae3f4
Marek Libra <marek.libra@gmail.com>
2020-04-01 13:21:55 +0200

    Move v2vvmware CRD under v2v.kubevirt.io API group
    Former group: kubevirt.io
    
    Signed-off-by: Marek Libra <mlibra@redhat.com>

--- kubevirt-vmware/pkg/apis/kubevirt/v1alpha1/doc.go
+++ kubevirt-vmware/pkg/apis/kubevirt/v1alpha1/doc.go
@@ -1,4 +1,4 @@
 // Package v1alpha1 contains API Schema definitions for the kubevirt v1alpha1 API group
 // +k8s:deepcopy-gen=package,register
-// +groupName=kubevirt.io
+// +groupName=v2v.kubevirt.io
 package v1alpha1

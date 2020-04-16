0240600781a1b96d97c2c50ce24972576188ade1
Matthew Arnold <marnold@redhat.com>
2020-04-15 22:49:55 -0400

    Avoid failing when VM name is a substring of another.

--- wrapper/hosts.py
+++ wrapper/hosts.py
@@ -609,7 +609,7 @@ class OpenstackHost(_BaseHost):
 
         servers = self._run_openstack(['server', 'list',
                                        '-f', 'value', '-c', 'Name'],
-                                      data)
+                                      data).split()
         if data['vm_name'] in servers:
             hard_error('VM with the name "%s" already exists on the '
                        'destination' % data['vm_name'])
